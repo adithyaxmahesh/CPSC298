40031fb752da26f6324e6935c2ee76603f8cd068
adithyaxmahesh <116032762+adithyaxmahesh@users.noreply.github.com>
2024-01-25 21:26:22 -0800

    Add files via upload

---
+++ hello_cin.cpp
@@ -0,0 +1,14 @@
+/// @hello_cin.cpp
+/// @Message reader
+/// @amahesh@chapman.edu
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+string strMessage;
+cout << "Please type a single word message" << endl;
+cin >> strMessage;
+cout << "Message: " << strMessage << endl;
+return 0;
+}
